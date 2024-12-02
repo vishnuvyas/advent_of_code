eb8e2f4f2bac52ba3fffef67327bc1b0dd57738b
vishnuvyas <vishnu@oii.com>
2024-12-01 20:17:35 -0800

    refactored common code into common.go

---
+++ aoc2024/day1/common.go
@@ -0,0 +1,47 @@
+package day1
+
+import (
+	"bufio"
+	"log"
+	"strings"
+	"os"
+	"strconv"
+)
+
+func ReadInputFile(filename string) ([]int, []int) {
+	list1 := make([]int, 0, 1000)
+	list2 := make([]int, 0, 1000)
+	
+	file , err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		parts := strings.SplitN(line, "\t", 2)
+		num1, err1  := strconv.Atoi(parts[0])
+		num2, err2 := strconv.Atoi(parts[1])
+		if err1 != nil ||  err2 != nil {
+			log.Fatal("Failed parsing on ", line)
+		}
+		list1 = append(list1, num1)
+		list2 = append(list2, num2)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return list1, list2
+}
+
+func IntAbs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
---
+++ aoc2024/day1/problem2.go
@@ -0,0 +1,24 @@
+package day1
+
+import (
+	"fmt"
+	"slices"
+)
+
+func Problem2Solution(filename string) {
+	list1, list2 := ReadInputFile(filename)
+	hist := make(map[int]int)
+
+	for i := 0; i < len(list2); i++ {
+		hist[list2[i]] += 1
+	}
+
+	slices.Sort(list1)
+	similarityScore := 0
+	for i := 0; i < len(list1); i++ {
+		individualScore := list1[i] * hist[list1[i]]
+		similarityScore += individualScore
+	}
+
+	fmt.Println("The similarity score is ", similarityScore)
+}
