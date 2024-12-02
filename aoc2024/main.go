fb7a693f4e2738e0887976e1d7f259cfe0a2884c
vishnuvyas <vishnu@oii.com>
2024-12-01 20:17:54 -0800

    refactor problem1 common code into common.go

--- aoc2024/main.go
+++ aoc2024/main.go
@@ -3,7 +3,7 @@ package main;
 import (
 	"fmt"
 	"os"
-	p1 "github.com/vishnuvyas/advent_of_code/aoc2024/day1" // alias  "day1/problem1" as p1
+	d1 "github.com/vishnuvyas/advent_of_code/aoc2024/day1" // alias  "day1/problem1" as p1
 )
 
 func main() {
@@ -11,6 +11,7 @@ func main() {
 	if len(os.Args) >= 2  {
 		input_file := os.Args[1]
 		fmt.Println("Input file is ", os.Args[1])
-		p1.Problem1Solution(input_file)
+		d1.Problem1Solution(input_file)
+		d1.Problem2Solution(input_file)
 	}
 }
